490a176796355fa633871638c5c5ab117a09a1f8
eylulnaz607 <eylulyemisken@gmail.com>
2026-04-21 02:44:03 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // GitHub Pages için en kritik satır!
+  output: 'export',
   images: {
-    unoptimized: true, // Statik sitede resimlerin hata vermemesi için gerekli
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Küçük kod hataları yayını engellemesin
+    unoptimized: true,
   },
 };
 
 export default nextConfig;
-
